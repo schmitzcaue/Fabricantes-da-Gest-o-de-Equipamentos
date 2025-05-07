09920f848e43a98e5bee6327dd82a9df00977455
caues <caues@SCHMITZNOT>
2025-05-06 21:16:04 -0300

    Add project files.

---
+++ Fabricantes da Gestão de Equipamentos ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fabricantes_da_Gestão_de_Equipamentos_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
